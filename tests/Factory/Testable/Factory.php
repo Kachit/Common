ac2e39cd38b1feff03602fc936a55ac0d49ee066
anton <kornilov@realweb.ru>
2014-11-20 18:51:16 +0300

    add test on factory

---
+++ tests/Factory/Testable/Factory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Factory
+ *
+ * @author antoxa <kornilov@realweb.ru>
+ */
+namespace Kachit\Common\Test\Factory\Testable;
+
+use Kachit\Common\Factory\AbstractFactory;
+
+class Factory extends AbstractFactory {
+
+    /**
+     * Init class prefix
+     *
+     * @return string
+     */
+    protected function initClassPrefix() {
+        // TODO: Implement initClassPrefix() method.
+    }
+} 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,6 @@
 <?php
- 
+/**
+ * @var Composer\Autoload\ClassLoader $autoloader
+ */
+$autoloader = require_once __DIR__ . '/../vendor/autoload.php';
+$autoloader->add('Kachit\Common\Test', __DIR__);
